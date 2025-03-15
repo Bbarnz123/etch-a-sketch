0e59ba3a1272be971f11b19fd6e13fcd6df4ce15
Bbarnz123 <barneyberesford@gmail.com>
2025-03-15 12:43:01 +0000

    update createGrid to work with removeGrid, create changeColour function for divs

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
 const container = document.querySelector("#container");
 const input = document.querySelector("input");
+const button = document.querySelector("button");
 
 let gridSize;
+let divs;
 
-if (input <= 100) {
-    gridSize = input.value;
-    gridSize *= gridSize;
-}
+button.addEventListener("click", () => {
+
+    removeGrid ();
+
+    if (input.value <= 100) {
+        gridSize = input.value;
+        createGrid ();
+        divs = document.querySelectorAll(".divs");
+
+        divs.forEach((item) => {
+            item.addEventListener("mouseenter", changeColour)
+        });
+    };
+
+    });
+
 
 function createGrid () {
+    for (let i = 0; i < gridSize; i++) {
+        const containerDiv = document.createElement("div");
+        containerDiv.style.display = "flex";
 
-    let divs = [];
+        for (let j = 0; j < gridSize; j++) {
+            const div = document.createElement("div");
+            div.style.boxSizing = "border-box";
+            div.style.width = 800 / input.value + "px";
+            div.style.height = 800 / input.value + "px";
+            div.style.border = "1px solid black";
+            div.className = "divs";
+            containerDiv.appendChild(div);
+        }
 
-    for (i = 0; i < gridSize; i++) {
-        divs[i] = document.createElement("div");
-        divs[i].style.border = "2px solid black";
-        container.appendChild(divs[i]);
+        container.appendChild(containerDiv);
     }
 }
+
+function removeGrid () {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+};
+
+
+
+function changeColour (event) {
+    event.target.style.backgroundColor = "#A9A9A9";
+}
+
+
+
 
 
